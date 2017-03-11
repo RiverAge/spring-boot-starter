b73f4339423d16ebcb84f61f90d8672331146ca9
李蒙@Mac Pro <riverage@163.com>
2017-03-11 22:16:20 +0800

    Mock Test get update create delete

--- src/test/java/com/euky/ws/web/api/GreetingControllerMocksTest.java
+++ src/test/java/com/euky/ws/web/api/GreetingControllerMocksTest.java
@@ -16,9 +16,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by euky on 2017/3/11.
@@ -32,7 +31,7 @@ public class GreetingControllerMocksTest extends AbstractControllerTest {
     private GreetingService greetingService;
 
     @InjectMocks
-    private  GreetingController controller;
+    private GreetingController controller;
 
     private Collection<Greeting> getEntityListStubData() {
         Collection<Greeting> list = new ArrayList<Greeting>();
@@ -70,6 +69,105 @@ public class GreetingControllerMocksTest extends AbstractControllerTest {
 
         Assert.assertEquals("failure - expected HTTP status 200", 200, status);
         Assert.assertTrue("failure - expected HTTP response body to have a value", content.trim().length() > 0);
-
     }
+
+    @Test
+    public void testGetGreetingNotFound() throws Exception {
+        Long id = Long.MAX_VALUE;
+
+        when(greetingService.findOne(id)).thenReturn(null);
+
+        String uri = "/api/greetings/{id}";
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get(uri, id).accept(MediaType.APPLICATION_JSON)).andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        verify(greetingService, times(1)).findOne(id);
+
+        Assert.assertEquals("failure - expected HTTP status 404", 404, status);
+        Assert.assertTrue("failure - expected HTTP response body to be empty", content.trim().length() == 0);
+    }
+
+    @Test
+    public void testCreateGreeting() throws Exception {
+        Greeting entity = getEntityStubData();
+
+        when(greetingService.create(any(Greeting.class))).thenReturn(entity);
+
+        String uri = "/api/greetings";
+        String inputJson = super.mapToJson(entity);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders
+                        .post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON).content(inputJson))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        verify(greetingService, times(1)).create(any(Greeting.class));
+
+        Assert.assertEquals("failure - expected HTTP status 201", 201, status);
+        Assert.assertTrue("failure - expected HTTP response body to have a value", content.trim().length() > 0);
+
+        Greeting createdEntiry = super.mapFromJson(content, Greeting.class);
+
+        Assert.assertNotNull("failure - expected entity not null", createdEntiry);
+        Assert.assertNotNull("failure - expected id attribute not null", createdEntiry.getId());
+        Assert.assertEquals("failure - expected text attribute match", entity.getText(), createdEntiry.getText());
+    }
+
+    @Test
+    public void testUpdateGreeting() throws Exception {
+        Greeting entity = getEntityStubData();
+        entity.setText(entity.getText() + "test");
+        Long id = new Long(1);
+
+        when(greetingService.update((any(Greeting.class)))).thenReturn(entity);
+
+        String uri = "/api/greetings/{id}";
+        String inputJson = super.mapToJson(entity);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders
+                        .put(uri, id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON).content(inputJson))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        verify(greetingService, times(1)).update(any(Greeting.class));
+
+        Assert.assertEquals("failure - expected HTTP status 200", 200, status);
+        Assert.assertTrue("failure - expected HTTP response body to have a value", content.trim().length() > 0);
+
+        Greeting updateEntity = super.mapFromJson(content, Greeting.class);
+
+        Assert.assertNotNull("failure - expected entity not null", updateEntity);
+        Assert.assertEquals("failure - expected id attributed unchanged", entity.getId(), updateEntity.getId());
+        Assert.assertEquals("failure - expected text attribute match", entity.getText(), updateEntity.getText());
+    }
+
+    @Test
+    public void testDeleteGreeting() throws Exception {
+        Long id = new Long(1);
+        String uri = "/api/greetings/{id}";
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.delete(uri, id)).andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        verify(greetingService, times(1)).delete(id);
+
+        Assert.assertEquals("failure - expected HTTP status 204", 204, status);
+        Assert.assertTrue("failure - expected HTTP response body to be empty", content.trim().length() == 0);
+    }
+
 }
+
