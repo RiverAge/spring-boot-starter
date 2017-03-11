ae615a67237e25d03aea9cba187222a35b7ff439
李蒙@Mac Pro <riverage@163.com>
2017-03-11 15:28:18 +0800

    Spring Cache Spring 缓存

--- src/main/java/com/euky/ws/service/GreetingServiceBean.java
+++ src/main/java/com/euky/ws/service/GreetingServiceBean.java
@@ -3,6 +3,9 @@ package com.euky.ws.service;
 import com.euky.ws.repository.GreetingRepository;
 import com.euky.ws.web.api.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -23,17 +26,20 @@ public class GreetingServiceBean implements  GreetingService {
     }
 
     @Override
+    @Cacheable(value = "greetings", key = "#id")
     public Greeting findOne(Long id) {
         return greetingRepository.findOne(id);
     }
 
     @Override
+    @CachePut(value = "greetings", key = "#result")
     public Greeting create(Greeting greeting) {
         if (greeting.getId() != null) return null;
         return greetingRepository.save(greeting);
     }
 
     @Override
+    @CachePut(value = "greetings", key = "#greeting.id")
     public Greeting update(Greeting greeting) {
         Greeting greetingPersisted = findOne(greeting.getId());
         if (greetingPersisted == null) {
@@ -43,8 +49,14 @@ public class GreetingServiceBean implements  GreetingService {
     }
 
     @Override
+    @CacheEvict(value = "greetings", key = "#id")
     public void delete(Long id) {
         greetingRepository.delete(id);
     }
 
+    @Override
+    @CacheEvict(value = "greetings", allEntries = true)
+    public void evictCache() {
+
+    }
 }
